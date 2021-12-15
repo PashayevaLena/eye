c971bd1de44640ff498837df9f864d04158fcc54
PashayevaLena <94740892+PashayevaLena@users.noreply.github.com>
2021-12-15 14:10:19 +0400

    Add files via upload

---
+++ 2/app.js
@@ -0,0 +1,17 @@
+// let x = "17";
+
+
+// if(x == 17){
+// alert("dogrudur");
+// }else if(x === 17){
+// alert("else if odendi")
+// } else if(x>16){
+
+
+let x = document.getElementById("name")
+
+if(x == Lena){
+    alert("true");
+}else {
+    alert("false")
+}
